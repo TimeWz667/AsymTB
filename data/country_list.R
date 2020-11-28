091ea092a91b49ab6ee359be364668e4cad1d62c
TimeWz667 <TimeWz667@gmail.com>
2020-11-28 15:26:18 +0000

    Add WHO CDR data

---
+++ data/country_list.R
@@ -0,0 +1,32 @@
+countries <- c(
+  KHM = "Cambodia",
+  ETH = "Ethiopia",
+  KEN = "Kenya",
+  LAO = "Lao People's Democratic Republic", 
+  MWI = "Malawi", 
+  PAK = "Pakistan", 
+  PHL = "Philippines", 
+  TZA = "United Republic of Tanzania", 
+  UGA = "Uganda", #
+  VNM = "Viet Nam", 
+  ZMB = "Zambia"
+)
+
+
+countries_cs <- c(
+  KHM = "Cambodia",
+  KEN = "Kenya",
+  MWI = "Malawi", 
+  PHL = "Philippines", 
+  TZA = "United Republic of Tanzania", 
+  UGA = "Uganda", #
+  ZMB = "Zambia"
+)
+
+
+countries_lab <- c(
+  "Cambodia", "Ethiopia", "Kenya", "Lao PDR", 
+  "Malawi", "Pakistan", "Philippines", 
+  "Tanzania", "Uganda" , "Viet Nam", "Zambia"
+)
+names(countries_lab) <- countries
